622ea867be3e4949691d9f0c049c52c30c08d110
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-14 19:16:54 +0200

    fix: hover in back arrow

--- src/app/dashboard/new-entry/page.tsx
+++ src/app/dashboard/new-entry/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function NewEntry() {
   return (
     <Modal>
-      <Link href='/dashboard' className='w-fit hover:ml-2 transition-all'>
-        <MoveLeft strokeWidth={1.5} />
+      <Link href='/dashboard' className='transition-all hover:pl-2'>
+        <MoveLeft strokeWidth={1.5} className='transition-all hover:ml-2' />
       </Link>
       <h1>New Entry</h1>
     </Modal>
