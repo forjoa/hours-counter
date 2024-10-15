e75ca13466326b63971b34e9d6343938784e8c40
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-16 01:41:21 +0200

    build: install types for bcrypt

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,9 +1,26 @@
+import { turso } from '@/core/db'
 import { NextResponse } from 'next/server'
+import bcrypt from 'bcrypt'
 
 export async function POST(request: Request) {
   const formData = await request.formData()
-  const mail = formData.get('mail')
-  const password = formData.get('password')
+  const mail = formData.get('mail') as string
+  const password = formData.get('password') as string
+
+  const { rows } = await turso.execute({
+    sql: 'SELECT * FROM users WHERE email = ?',
+    args: [mail],
+  })
+
+  const userInDb = rows[0]
+
+  if (!userInDb) {
+    return NextResponse.json({ message: 'No user found', success: false })
+  }
+
+  if (!(await bcrypt.compare(password, userInDb.password as string))) {
+    return NextResponse.json({ message: 'Wrong password', success: false })
+  }
 
   return NextResponse.json({ mail, password })
 }
