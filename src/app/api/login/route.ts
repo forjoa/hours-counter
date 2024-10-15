b94e02089ea81ae44b9f2fa4a72e7b42e662b68e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-15 20:07:47 +0200

    build: getting data when logging

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  const { email, password } = await request.json()
-  console.log(email, password)
+  const formData = await request.formData()
+  const mail = formData.get('mail')
+  const password = formData.get('password')
 
-  return NextResponse.json({ email, password })
+  return NextResponse.json({ mail, password })
 }
