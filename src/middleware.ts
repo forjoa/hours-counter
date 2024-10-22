d3e140b6cdd6ab2debff6ce06378e0cfcc5785b5
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-22 20:42:38 +0200

    todo: fix runtime crypto module

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import jwt from 'jsonwebtoken'
+import { cookies } from 'next/headers'
 
 // Esta función puede ser marcada como `async` si se usa `await` dentro
 export function middleware(request: NextRequest) {
   // Asumimos que tienes alguna forma de verificar si el usuario está autenticado
   // Por ejemplo, podrías verificar una cookie o un token en el header
-  const isAuthenticated = checkAuthentication(request)
+  const isAuthenticated = checkAuthentication()
 
   // Si el usuario no está autenticado y la ruta no es /login, redirige a /login
   if (!isAuthenticated && !request.nextUrl.pathname.startsWith('/login')) {
@@ -17,11 +19,20 @@ export function middleware(request: NextRequest) {
 }
 
 // Esta función es un placeholder. Deberías implementarla según tu lógica de autenticación
-function checkAuthentication(request: NextRequest): boolean {
-  const token = request.cookies.get('auth-token')
+function checkAuthentication(): boolean {
+  const cookiesStore = cookies()
+  const token = cookiesStore.get('user')?.value
+
+  if (!token) {
+    return false
+  }
+  console.log(token.split("\"").join("")); 
+
+  const payload = jwt.verify(token.split("\"").join(""), process.env.SIGNATURE as string)
+
   // Aquí iría tu lógica para verificar si el usuario está autenticado
   // Por ejemplo, podrías verificar una cookie o un token en el header
-  console.log(token)
+  console.log(payload)
   return true // Cambia esto según tu implementación
 }
 
