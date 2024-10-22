893777c7dac2231f0ae018d19860045f0bb572f4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-22 19:57:04 +0200

    feat: save jwt with user information

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,12 +1,16 @@
 import { turso } from '@/core/db'
 import { NextResponse } from 'next/server'
 import bcrypt from 'bcrypt'
-import { ApiResponse } from '@/core/types'
+import { ApiResponse, User } from '@/core/types'
+import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken'
 
 export async function POST(
   request: Request
 ): Promise<NextResponse<ApiResponse>> {
+  const cookiesStore = cookies()
   const formData = await request.formData()
+
   const mail = formData.get('mail') as string
   const password = formData.get('password') as string
 
@@ -15,7 +19,7 @@ export async function POST(
     args: [mail],
   })
 
-  const userInDb = rows[0]
+  const userInDb = rows[0] as unknown as User
 
   if (!userInDb) {
     return NextResponse.json({ message: 'No user found', success: false })
@@ -24,6 +28,12 @@ export async function POST(
   if (!(await bcrypt.compare(password, userInDb.password as string))) {
     return NextResponse.json({ message: 'Wrong password', success: false })
   }
+
+  delete userInDb.password
+
+  const token = jwt.sign(userInDb, process.env.SIGNATURE as string)
+
+  cookiesStore.set('user', JSON.stringify(token))
 
   return NextResponse.json({ message: 'Login successful', success: true })
 }
