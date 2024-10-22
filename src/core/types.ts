c0925f04507cca7d655397fd21617aa48c1feaa4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-22 19:57:14 +0200

    build: user type

--- src/core/types.ts
+++ src/core/types.ts
@@ -2,3 +2,13 @@ export interface ApiResponse {
   message: string
   success: boolean
 }
+
+export interface User {
+  userid?: number
+  name: string
+  lastname: string
+  email: string
+  password?: string
+  createdat?: string
+  updatedat?: string
+}
