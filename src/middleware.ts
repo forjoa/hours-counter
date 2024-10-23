09cfc505c6afd9c2b023f3ab465c686dccbe561e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-23 11:13:23 +0200

    build: middleware working correctly

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,58 @@
+'use server'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
+import { jwtVerify } from 'jose'
+import { turso } from './core/db'
+import { User } from './core/types'
 
-// Esta función puede ser marcada como `async` si se usa `await` dentro
-export function middleware(request: NextRequest) {
-  // Asumimos que tienes alguna forma de verificar si el usuario está autenticado
-  // Por ejemplo, podrías verificar una cookie o un token en el header
-  const isAuthenticated = checkAuthentication()
+export async function middleware(request: NextRequest) {
+  const isAuthenticated = await checkAuthentication(request)
 
-  // Si el usuario no está autenticado y la ruta no es /login, redirige a /login
-  if (!isAuthenticated && !request.nextUrl.pathname.startsWith('/login')) {
+  if (!isAuthenticated) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  // Si el usuario está autenticado o la ruta es /login, permite que la solicitud continúe
   return NextResponse.next()
 }
 
-// Esta función es un placeholder. Deberías implementarla según tu lógica de autenticación
-function checkAuthentication(): boolean {
+async function checkAuthentication(request: NextRequest): Promise<boolean> {
   const cookiesStore = cookies()
   const token = cookiesStore.get('user')?.value
+
+  if (
+    request.nextUrl.pathname.startsWith('/api/login') ||
+    request.nextUrl.pathname.startsWith('/login')
+  ) {
+    return true
+  }
 
   if (!token) {
     return false
   }
-  console.log(token.split("\"").join("")); 
 
-  const payload = jwt.verify(token.split("\"").join(""), process.env.SIGNATURE as string)
+  try {
+    const secret = new TextEncoder().encode(process.env.SIGNATURE)
+    const { payload } = await jwtVerify(token.split('"').join(''), secret)
 
-  // Aquí iría tu lógica para verificar si el usuario está autenticado
-  // Por ejemplo, podrías verificar una cookie o un token en el header
-  console.log(payload)
-  return true // Cambia esto según tu implementación
+    const user = payload as unknown as User
+
+    const { rows } = await turso.execute({
+      sql: 'SELECT * FROM users WHERE email = ? AND name = ?',
+      args: [user.email, user.name],
+    })
+
+    if (rows.length === 0) {
+      return false
+    }
+
+    return true
+  } catch (error) {
+    console.error('Token verification failed', error)
+    return false
+  }
 }
 
-// Configura el middleware para que se ejecute en todas las rutas excepto en /login
 export const config = {
   matcher: ['/((?!login).*)'],
 }
