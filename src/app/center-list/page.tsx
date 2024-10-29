9ca04cafdefadb1ccfd7fe2548541b2eb18b09d7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-29 13:52:32 +0100

    feat: show work center list

--- src/app/center-list/page.tsx
+++ src/app/center-list/page.tsx
@@ -1,9 +1,87 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import Main from '@/components/ui/Main'
+import { WorkCenter } from '@/core/types'
 
 export default function NewEntry() {
+  const [centers, setCenters] = useState<WorkCenter[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchCenters = async () => {
+      try {
+        const response = await fetch('/api/get-centers', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        setCenters(data)
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'Error fetching data')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchCenters()
+  }, [])
+
+  if (isLoading)
+    return (
+      <Main>
+        <div>Cargando...</div>
+      </Main>
+    )
+  if (error)
+    return (
+      <Main>
+        <div>Error: {error}</div>
+      </Main>
+    )
+
   return (
     <Main>
-      <h1>Lista de centros de trabajo</h1>
+      <h1 className='font-bold'>Centros de trabajo</h1>
+      <span className='text-zinc-500'>
+        Esta es toda tu lista de centros de trabajo
+      </span>
+
+      <div className='overflow-x-auto rounded-lg border border-gray-200 mt-2'>
+        <table className='min-w-full divide-y-2 divide-gray-200 bg-white text-sm'>
+          <thead className='ltr:text-left rtl:text-right'>
+            <tr>
+              <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                Centro
+              </th>
+              <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                Creación
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className='divide-y divide-gray-200'>
+            {centers.map((center: WorkCenter) => (
+              <tr key={center.workcenterid}>
+                <td className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  {center.name}
+                </td>
+                <td className='whitespace-nowrap px-4 py-2 text-gray-700'>
+                  {new Date(center.createdat).toLocaleDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Main>
   )
 }
