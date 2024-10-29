ac4e062b135934022392fb29e660aa8de8499018
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-29 12:50:42 +0100

    fix> add user id to save work center

--- src/app/api/add-center/route.ts
+++ src/app/api/add-center/route.ts
@@ -1,16 +1,26 @@
-import { turso } from "@/core/db"
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { turso } from '@/core/db'
+import { User } from '@/core/types'
+import { jwtVerify } from 'jose'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
   const cookiesStore = await cookies()
   const formData = await request.formData()
+  const token = cookiesStore.get('user')?.value
+  const newCenter = formData.get('center') as string
 
-  const newCenter = formData.get('center') as string 
+  const secret = new TextEncoder().encode(process.env.SIGNATURE)
+
+  if (!token) return
+
+  const { payload } = await jwtVerify(token.split('"').join(''), secret)
+
+  const user = payload as unknown as User
 
   const { rows } = await turso.execute({
-    sql: 'SELECT * FROM workcenter WHERE LOWER(name) = LOWER(?)', 
-    args: [newCenter],
+    sql: 'SELECT * FROM workcenter WHERE LOWER(name) = LOWER(?) AND userid = ?',
+    args: [newCenter, user.userid as number],
   })
 
   if (rows.length > 0) {
@@ -19,8 +29,8 @@ export async function POST(request: Request) {
   }
 
   await turso.execute({
-    sql: 'INSERT INTO workcenter (name) VALUES (?)',
-    args: [newCenter],
+    sql: 'INSERT INTO workcenter (name, userid) VALUES (?, ?)',
+    args: [newCenter, user.userid as number],
   })
 
   cookiesStore.set('centerError', '', { maxAge: 30 })
