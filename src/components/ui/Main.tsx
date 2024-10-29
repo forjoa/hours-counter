cae9bb11393151dc3268c56a8c45beb7a3d1992f
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-29 11:19:49 +0100

    refactor: update media queries

--- src/components/ui/Main.tsx
+++ src/components/ui/Main.tsx
@@ -1,5 +1,9 @@
 import { PropsWithChildren } from 'react'
 
 export default function Main({ children }: PropsWithChildren) {
-  return <main className='mt-6 mb-6 mr-6 bg-white rounded p-6 '>{children}</main>
+  return (
+    <main className='md:mt-6 mt-16 md:mb-6 mb-12 md:mr-6 mr-4 ml-4 md:ml-0 bg-white rounded p-6 '>
+      {children}
+    </main>
+  )
 }
