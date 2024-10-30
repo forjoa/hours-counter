7c64f0e9c836226c6789b180e85bc7c340e96360
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-30 16:14:34 +0100

    build: endpoint to save hours

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -1,8 +1,42 @@
-import { NextResponse } from 'next/server'
+import { turso } from '@/core/db'
+import { User } from '@/core/types'
+import { jwtVerify } from 'jose'
+import { cookies } from 'next/headers'
+
+const QUERY =
+  'INSERT INTO hours (day, starttime, endtime, workcenterid, userid, annotations) VALUES (?, ?, ?, ?, ?, ?)'
 
 export async function POST(request: Request) {
-  const { id, hours } = await request.json()
-  console.log(id, hours)
+  const cookiesStore = await cookies()
+  const token = cookiesStore.get('user')?.value
 
-  return NextResponse.json({ id, hours })
+  const formData = await request.formData()
+
+  const day = formData.get('date') as string
+  const starthour = formData.get('starthour') as string
+  const finishhour = formData.get('finishhour') as string
+  const workcenterid = formData.get('workcenter') as string
+  const annotations = formData.get('annotations') as string
+
+  const secret = new TextEncoder().encode(process.env.SIGNATURE)
+
+  if (!token) return
+
+  const { payload } = await jwtVerify(token.split('"').join(''), secret)
+
+  const user = payload as unknown as User
+
+  await turso.execute({
+    sql: QUERY,
+    args: [
+      day,
+      starthour,
+      finishhour,
+      workcenterid,
+      user.userid as number,
+      annotations,
+    ],
+  })
+
+  return Response.redirect(new URL('/', request.url))
 }
