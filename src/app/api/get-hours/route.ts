dc2d29be0b08fb08edd3f485897f0ddc3d240bb0
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 16:29:08 +0100

    build: complete login to get hours

--- src/app/api/get-hours/route.ts
+++ src/app/api/get-hours/route.ts
@@ -1,21 +1,34 @@
-import {NextRequest, NextResponse} from "next/server";
-import {getUserFromToken} from "@/core/getToken";
-import {turso} from "@/core/db";
+import { NextRequest, NextResponse } from "next/server";
+import { getUserFromToken } from "@/core/getToken";
+import { turso } from "@/core/db";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    const {month} = body;
+    const { month, workcenterid } = body;
 
     const user = await getUserFromToken()
 
     if (!user) return
 
-    const query = 'SELECT h.*, wc.name FROM hours h JOIN workcenter wc ON h.workcenterid = wc.workcenterid WHERE h.userid = ? AND strftime(\'%Y-%m\', day) = ?'
+    let query = `
+        SELECT h.*, wc.name 
+        FROM hours h 
+        JOIN workcenter wc ON h.workcenterid = wc.workcenterid 
+        WHERE h.userid = ? 
+        AND h.day LIKE ?
+    `;
 
-    const {rows} = await turso.execute({
+    let args = [user.userid as number, `${month}%`];
+
+    if (workcenterid) {
+        query += ' AND h.workcenterid = ?';
+        args.push(workcenterid);
+    }
+
+    const { rows } = await turso.execute({
         sql: query,
-        args: [user.userid, month ? month : 'now']
-    })
+        args
+    });
 
-    return NextResponse.json(rows)
+    return NextResponse.json(rows);
 }
