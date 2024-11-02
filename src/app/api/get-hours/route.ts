e642025302126823cf06335669abede91dfb7e98
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 15:37:44 +0100

    build: logic response to get hours

--- src/app/api/get-hours/route.ts
+++ src/app/api/get-hours/route.ts
@@ -1,8 +1,21 @@
 import {NextRequest, NextResponse} from "next/server";
+import {getUserFromToken} from "@/core/getToken";
+import {turso} from "@/core/db";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const {month} = body;
 
-    return NextResponse.json({month})
+    const user = await getUserFromToken()
+
+    if (!user) return
+
+    const query = 'SELECT h.*, wc.name FROM hours h JOIN workcenter wc ON h.workcenterid = wc.workcenterid WHERE h.userid = ? AND strftime(\'%Y-%m\', day) = ?'
+
+    const {rows} = await turso.execute({
+        sql: query,
+        args: [user.userid, month ? month : 'now']
+    })
+
+    return NextResponse.json(rows)
 }
