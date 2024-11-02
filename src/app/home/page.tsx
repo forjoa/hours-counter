554eba49af5e538356c3275f89ebcf2c423a0457
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 16:55:47 +0100

    refactor: update table style

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,64 +5,66 @@ import {Hour} from '@/core/types'
 
 function HoursTable({hours}: { hours: Hour[] }) {
     return (
-        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-            <thead className="ltr:text-left rtl:text-right">
-            <tr>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Día</th>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Horario</th>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Total</th>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Centro</th>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Anotaciones</th>
-            </tr>
-            </thead>
+        <div className='overflow-x-auto rounded-lg border border-gray-200 mt-2'>
+            <table className='min-w-full divide-y-2 divide-gray-200 bg-white text-sm'>
+                <thead className="ltr:text-left rtl:text-right">
+                <tr>
+                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Día</th>
+                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Horario</th>
+                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Total</th>
+                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Centro</th>
+                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Anotaciones</th>
+                </tr>
+                </thead>
 
-            <tbody className="divide-y divide-gray-200">
-            {hours && hours.length > 0 && hours.map((hour: Hour) => {
-                const [startHours, startMinutes] = hour.starttime.split(':').map(Number)
-                const [endHours, endMinutes] = hour.endtime.split(':').map(Number)
+                <tbody className="divide-y divide-gray-200">
+                {hours && hours.length > 0 && hours.map((hour: Hour) => {
+                    const [startHours, startMinutes] = hour.starttime.split(':').map(Number)
+                    const [endHours, endMinutes] = hour.endtime.split(':').map(Number)
 
-                const startTotalMinutes = startHours * 60 + startMinutes
-                const endTotalMinutes = endHours * 60 + endMinutes
+                    const startTotalMinutes = startHours * 60 + startMinutes
+                    const endTotalMinutes = endHours * 60 + endMinutes
 
-                const diffHours = (endTotalMinutes - startTotalMinutes) / 60
+                    const diffHours = (endTotalMinutes - startTotalMinutes) / 60
 
-                const dias = [
-                    'Domingo',
-                    'Lunes',
-                    'Martes',
-                    'Miércoles',
-                    'Jueves',
-                    'Viernes',
-                    'Sábado'
-                ]
+                    const dias = [
+                        'Domingo',
+                        'Lunes',
+                        'Martes',
+                        'Miércoles',
+                        'Jueves',
+                        'Viernes',
+                        'Sábado'
+                    ]
 
-                const date = new Date(hour.day)
-                const dia = dias[date.getDay()]
-                const numero = date.getDate()
-                const formattedDate = `${dia} ${numero}`
+                    const date = new Date(hour.day)
+                    const dia = dias[date.getDay()]
+                    const numero = date.getDate()
+                    const formattedDate = `${dia} ${numero}`
 
-                return (
-                    <tr key={hour.hourid}>
-                        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                            {formattedDate}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                            {hour.starttime} - {hour.endtime}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                            {diffHours.toFixed(1)}h
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                            {hour.name}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                            {hour.annotations ? hour.annotations : 'No hay anotación'}
-                        </td>
-                    </tr>
-                )
-            })}
-            </tbody>
-        </table>
+                    return (
+                        <tr key={hour.hourid}>
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                {formattedDate}
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {hour.starttime} - {hour.endtime}
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {diffHours.toFixed(1)}h
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {hour.name}
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {hour.annotations ? hour.annotations : 'No hay anotación'}
+                            </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
@@ -88,7 +90,7 @@ function HoursContent() {
     }, [searchParams]);
 
     return (
-        <div className="overflow-x-auto rounded-lg border border-gray-200 p-2 mt-4">
+        <div className="overflow-x-auto rounded border border-gray-200 p-2 mt-4">
             <header>
                 <h1 className='font-bold'>Listado de horas por centro</h1>
                 <span className='text-zinc-500'>
