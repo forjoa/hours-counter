2acf9016a26da4dfdacb239dea3293bbc9667039
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 16:46:43 +0100

    fix: implement suspense component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,80 @@
 'use client'
 import {useSearchParams} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useEffect, useState, Suspense} from "react";
 import {Hour} from '@/core/types'
 
-export default function Home() {
-    const pathname = useSearchParams();
+// Componente de tabla separado
+function HoursTable({hours}: { hours: Hour[] }) {
+    return (
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+            <thead className="ltr:text-left rtl:text-right">
+            <tr>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Día</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Horario</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Total</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Centro</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Anotaciones</th>
+            </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200">
+            {hours && hours.length > 0 && hours.map((hour: Hour) => {
+                const [startHours, startMinutes] = hour.starttime.split(':').map(Number)
+                const [endHours, endMinutes] = hour.endtime.split(':').map(Number)
+
+                const startTotalMinutes = startHours * 60 + startMinutes
+                const endTotalMinutes = endHours * 60 + endMinutes
+
+                const diffHours = (endTotalMinutes - startTotalMinutes) / 60
+
+                const dias = [
+                    'Domingo',
+                    'Lunes',
+                    'Martes',
+                    'Miércoles',
+                    'Jueves',
+                    'Viernes',
+                    'Sábado'
+                ]
+
+                const date = new Date(hour.day)
+                const dia = dias[date.getDay()]
+                const numero = date.getDate()
+                const formattedDate = `${dia} ${numero}`
+
+                return (
+                    <tr key={hour.hourid}>
+                        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            {formattedDate}
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                            {hour.starttime} - {hour.endtime}
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                            {diffHours.toFixed(1)}h
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                            {hour.name}
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                            {hour.annotations ? hour.annotations : 'No hay anotación'}
+                        </td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+    )
+}
+
+// Componente que maneja los datos
+function HoursContent() {
+    const searchParams = useSearchParams();
     const [hours, setHours] = useState<Hour[]>()
 
     useEffect(() => {
         const fetchHours = async () => {
-            const month = pathname.get('month') != null ? pathname.get('month') : new Date().toISOString().slice(0, 7)
+            const month = searchParams.get('month') != null ? searchParams.get('month') : new Date().toISOString().slice(0, 7)
             const res = await fetch(`/api/get-hours`, {
                 method: 'POST',
                 headers: {
@@ -22,73 +87,30 @@ export default function Home() {
         }
 
         fetchHours().then()
-    }, [pathname]);
+    }, [searchParams]);
 
     return (
         <div className="overflow-x-auto rounded-lg border border-gray-200 p-2">
             <header>
                 <h1 className='font-bold'>Listado de horas por centro</h1>
-                <span
-                    className='text-zinc-500'>Las horas mostradas son las del centro y mes seleccionados.</span>
+                <span className='text-zinc-500'>
+                    Las horas mostradas son las del centro y mes seleccionados.
+                </span>
             </header>
-            <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-                <thead className="ltr:text-left rtl:text-right">
-                <tr>
-                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Día</th>
-                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Horario</th>
-                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Total</th>
-                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Centro</th>
-                    <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Anotaciones</th>
-                </tr>
-                </thead>
-
-                <tbody className="divide-y divide-gray-200">
-                {hours && hours.length > 0 && hours.map((hour: Hour) => {
-                    const [startHours, startMinutes] = hour.starttime.split(':').map(Number)
-                    const [endHours, endMinutes] = hour.endtime.split(':').map(Number)
-
-                    const startTotalMinutes = startHours * 60 + startMinutes
-                    const endTotalMinutes = endHours * 60 + endMinutes
-
-                    const diffHours = (endTotalMinutes - startTotalMinutes) / 60
-
-                    const dias = [
-                        'Domingo',
-                        'Lunes',
-                        'Martes',
-                        'Miércoles',
-                        'Jueves',
-                        'Viernes',
-                        'Sábado'
-                    ]
-
-                    const date = new Date(hour.day)
-                    const dia = dias[date.getDay()]
-                    const numero = date.getDate()
-                    const formattedDate = `${dia} ${numero}`
-
-                    return (
-                        <tr key={hour.hourid}>
-                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                                {formattedDate}
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {hour.starttime} - {hour.endtime}
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {diffHours.toFixed(1)}h
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {hour.name}
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {hour.annotations ? hour.annotations : 'No hay anotación'}
-                            </td>
-                        </tr>
-                    )
-                })}
-                </tbody>
-            </table>
+            <HoursTable hours={hours ?? []}/>
         </div>
+    )
+}
+
+// Componente principal
+export default function Home() {
+    return (
+        <Suspense fallback={
+            <div className="overflow-x-auto rounded-lg border border-gray-200 p-2">
+                <p>Cargando datos...</p>
+            </div>
+        }>
+            <HoursContent/>
+        </Suspense>
     )
 }
