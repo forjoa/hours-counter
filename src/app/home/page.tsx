b98a7c931dde03a39ebe953d569e6e126da9e3f3
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 14:53:17 +0100

    build: get URL search params

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import {useSearchParams} from "next/navigation";
+
 export default function Home() {
+    const pathname = useSearchParams();
     return (
         <div>
             <span>here we need to say the current url</span>
+            <span>{(pathname.get('month') == null).toString()}</span>
         </div>
     )
 }
