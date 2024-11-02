f94186d15f8f3be7136c0ddcb94b8f0829921cf6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-02 14:35:59 +0100

    refactor: clean page component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,20 +1,7 @@
-import Main from '@/components/ui/Main'
-
 export default function Home() {
     return (
-        <Main>
-            <div className='border border-zinc-200 p-2 rounded'>
-                <h1 className='font-bold'>Promedio de horas semanales</h1>
-                <span
-                    className='text-zinc-500'>Este es el promedio de las horas que has trabajado a la semana este mes.</span>
-                {/*number of average weekly hours*/}
-            </div>
-            <div className='border border-zinc-200 p-2 rounded'>
-                <h1 className='font-bold'>Total de horas este mes</h1>
-                <span
-                    className='text-zinc-500'>Número total de horas trabajas en el mes.</span>
-                {/*number of total monthly hours*/}
-            </div>
-        </Main>
+        <div>
+            <span>here we need to say the current url</span>
+        </div>
     )
 }
