d6d27aca721df673555d381e8231cc9b34a2e6db
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-03 19:33:24 +0100

    feat: get average and total hours

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,13 +1,31 @@
 import Main from "@/components/ui/Main";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
+import {getMonthlyHours} from "@/core/getMonthlyHours";
 
-export default function Layout({
-                                   children,
-                               }: Readonly<{
+export default async function Layout({
+                                         children,
+                                     }: Readonly<{
     children: ReactNode
 }>) {
+    const hours = await getMonthlyHours()
+
+    let weeklyAverage
+    let totalHours = 0
+
+    hours?.map(hour => {
+        const [startHours, startMinutes] = hour.starttime.split(':').map(Number)
+        const [endHours, endMinutes] = hour.endtime.split(':').map(Number)
+
+        const startTotalMinutes = startHours * 60 + startMinutes
+        const endTotalMinutes = endHours * 60 + endMinutes
+
+        const diffHours = (endTotalMinutes - startTotalMinutes) / 60
+        totalHours += diffHours
+        weeklyAverage = (totalHours / 7).toFixed(1)
+    })
     return (
         <Main>
+            {children}
             <section className='border border-zinc-200 p-4 shadow rounded mt-4'>
                 <header>
                     <h1 className='font-bold'>Promedio de horas semanales</h1>
@@ -15,7 +33,7 @@ export default function Layout({
                 <main>
                 <span
                     className='text-zinc-500'>Este es el promedio de las horas que has trabajado a la semana este mes.</span>
-                    {/*number of average weekly hours*/}
+                    <p className='w-full text-center text-2xl font-bold'>{weeklyAverage}</p>
                 </main>
             </section>
             <section className='border border-zinc-200 p-4 shadow rounded mt-4'>
@@ -25,10 +43,9 @@ export default function Layout({
                 <main>
                 <span
                     className='text-zinc-500'>Número total de horas trabajas en el mes.</span>
-                    {/*number of total monthly hours*/}
+                    <p className='w-full text-center text-2xl font-bold'>{totalHours}</p>
                 </main>
             </section>
-            {children}
         </Main>
     )
 }
