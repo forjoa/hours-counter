b737209984bec534493e209c12e9e1b636405c70
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-04 19:08:15 +0100

    style: formatted w/ lint

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,36 +1,36 @@
 export interface ApiResponse {
-    message: string
-    success: boolean
+  message: string
+  success: boolean
 }
 
 export interface User {
-    userid?: number
-    name: string
-    lastname: string
-    email: string
-    password?: string
-    createdat?: string
-    updatedat?: string
+  userid?: number
+  name: string
+  lastname: string
+  email: string
+  password?: string
+  createdat?: string
+  updatedat?: string
 }
 
 export interface WorkCenter {
-    workcenterid?: number
-    name: string
-    userid: number
-    createdat: string
+  workcenterid?: number
+  name: string
+  userid: number
+  createdat: string
 }
 
 export interface WorkShift {
-    hourid: number;
-    day: string;
-    starttime: string;
-    endtime: string;
-    workcenterid: number;
-    userid: number;
-    annotations?: string;
-    createdat: string;
+  hourid: number
+  day: string
+  starttime: string
+  endtime: string
+  workcenterid: number
+  userid: number
+  annotations?: string
+  createdat: string
 }
 
 export interface Hour extends WorkShift {
-    name: string;
+  name: string
 }
