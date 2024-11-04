4ef68a1b059667f120183b6610192e48398ef7a6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-04 19:07:50 +0100

    style: formatted w/ lint

--- src/core/getMonthlyHours.ts
+++ src/core/getMonthlyHours.ts
@@ -1,16 +1,16 @@
-import {getUserFromToken} from "@/core/getToken";
-import {turso} from "@/core/db";
-import {WorkShift} from "@/core/types";
+import { getUserFromToken } from '@/core/getToken'
+import { turso } from '@/core/db'
+import { WorkShift } from '@/core/types'
 
 export async function getMonthlyHours() {
-    const user = await getUserFromToken()
+  const user = await getUserFromToken()
 
-    if (!user) return
+  if (!user) return
 
-    const {rows} = await turso.execute({
-        sql: 'SELECT * FROM hours WHERE userid = ? AND strftime(\'%Y-%m\', day) = strftime(\'%Y-%m\', \'now\')',
-        args: [user.userid as number]
-    })
+  const { rows } = await turso.execute({
+    sql: "SELECT * FROM hours WHERE userid = ? AND strftime('%Y-%m', day) = strftime('%Y-%m', 'now')",
+    args: [user.userid as number],
+  })
 
-    return rows as unknown as WorkShift[]
+  return rows as unknown as WorkShift[]
 }
