ca0f985e8ab4dd9757bf52f4c4aa2f966eb9d4ad
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-05 19:12:22 +0100

    build: method to get center information

---
+++ src/core/getHour.ts
@@ -0,0 +1,11 @@
+import { turso } from './db'
+import { WorkCenter } from './types'
+
+export async function getHour(id: string) {
+  const { rows } = await turso.execute({
+    sql: 'SELECT * FROM workcenter WHERE workcenterid = ?',
+    args: [id],
+  })
+
+  return (rows[0]) as unknown as WorkCenter
+}
